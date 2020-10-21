a27930dc9344e82ad6de85ec238e776d212d8878
MarySerna <mary.serna.8903@gmail.com>
2020-10-20 21:36:03 -0500

    Python async

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+asyncio.Task
+"""
+
+from asyncio import Task, create_task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """
+    Tasks
+    """
+    time = create_task(wait_random(max_delay))
+    return time
