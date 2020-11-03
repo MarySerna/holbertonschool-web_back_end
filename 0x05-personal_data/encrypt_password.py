ec90e683d2cf738e2b2a4a0d58145f9c13c525cb
MarySerna <mary.serna.8903@gmail.com>
2020-11-02 21:08:08 -0500

    encrypted

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+User passwords should NEVER
+be stored in plain text in a database
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a salted, hashed password, which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
