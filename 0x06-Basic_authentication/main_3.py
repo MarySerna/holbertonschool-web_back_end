9fb4e09c14308805f3603b70cbdcba17eabd64fe
MarySerna <mary.serna.8903@gmail.com>
2020-11-10 20:13:06 -0500

    fixed main

--- 0x06-Basic_authentication/main_3.py
+++ 0x06-Basic_authentication/main_3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Main 3
+"""
+Main 3
 """
 from api.v1.auth.basic_auth import BasicAuth
 
@@ -10,6 +11,6 @@ print(a.decode_base64_authorization_header(89))
 print(a.decode_base64_authorization_header("Holberton School"))
 print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
 print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
-print(a.decode_base64_authorization_header
-        (a.extract_base64_authorization_header
-        ("Basic SG9sYmVydG9uIFNjaG9vbA==")))
+print(a.decode_base64_authorization_header(
+    a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+)
