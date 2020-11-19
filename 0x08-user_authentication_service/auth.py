765680aa6714c3961cdad4a2b19e5e280cbf2de3
MarySerna <mary.serna.8903@gmail.com>
2020-11-18 21:52:25 -0500

    user authentication service

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,9 +3,14 @@
 Authentication module
 """
 
-import bcrypt
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from db import DB
-from typing import TypeVar
+from user import User
+import bcrypt
 import uuid
 
 
