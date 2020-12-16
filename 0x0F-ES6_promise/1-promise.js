049d5da901073917ef91e90ef6d16de4f9cff4ec
MarySerna <mary.serna.8903@gmail.com>
2020-12-16 13:54:20 -0500

    Primises

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -5,8 +5,7 @@ function getFullResponseFromAPI(success) {
         status: 200,
         body: 'Success',
       });
-    }
-    else {
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
