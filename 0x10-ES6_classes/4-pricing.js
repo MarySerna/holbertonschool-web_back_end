c58d7975ab146a5e0dda9177ae44329434a6e8ad
MarySerna <mary.serna.8903@gmail.com>
2020-12-20 22:07:53 -0500

    classes

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,32 +1,36 @@
 import Currency from './3-currency';
 
-export default class Pricing {
+class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
+  // amount
   get amount() {
     return this._amount;
   }
 
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+  }
+
+  // currency
   get currency() {
     return this._currency;
   }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  set currency(newCurrency) {
-    this._currency = newCurrency;
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this.amount} (${this.currency.displayFullCurrency()})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
+
+export default Pricing;
