b35678daa77f2513432f2ab4b5befd9280465bda
MarySerna <mary.serna.8903@gmail.com>
2020-12-20 22:54:55 -0500

    classes

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -13,9 +13,6 @@ export class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -28,9 +25,12 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this.holbertonClass;
   }
 }
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
@@ -39,5 +39,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-
-export default listOfStudents;
+module.exports = listOfStudents;
