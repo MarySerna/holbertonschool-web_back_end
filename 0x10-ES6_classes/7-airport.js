dd680de1ef79745bf83f60992bb150176ae862b4
MarySerna <mary.serna.8903@gmail.com>
2020-12-20 22:27:59 -0500

    classes

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,10 +1,12 @@
-export default class Airport {
+class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  get[Symbol.toStringTag]() {
-    return JSON.stringify(Airport);
+  get [Symbol.toStringTag]() {
+    return this._code;
   }
 }
+
+export default Airport;
