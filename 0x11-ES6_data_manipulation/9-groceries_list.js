7ee89e07bb0bff173c0bb70a9ec6826362683333
MarySerna <mary.serna.8903@gmail.com>
2020-12-25 20:59:58 -0500

    data manipulation

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,5 @@
 export default function groceriesList() {
-   const groceryArray = [
-     ['Apples', 10],
-     ['Tomatoes', 10],
-     ['Pasta', 1],
-     ['Rice', 1],
-     ['Banana', 5]
-  ];
+  const groceryArray = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
   const groceryMap = new Map();
   for (const el of groceryArray) groceryMap.set(el[0], el[1]);
   return groceryMap;
