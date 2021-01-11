5d45738cabbb9e2da7fe3d767c4079a4d025fc77
MarySerna <mary.serna.8903@gmail.com>
2021-01-11 20:28:28 +0000

    utils

--- 0x12-Node_JS_basic/full_server/utils.js
+++ 0x12-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,32 @@
 const fs = require('fs');
 
-const readDatabase = (path) => {
-  const promise = (res, rej) => {
-    fs.readFile(path, (err, data) => {
-      if (err) rej(Error('Cannot load the database'));
-      if (data) {
-        let dt = data.toString().split('\n');
-        dt = dt.slice(1, dt.length - 1);
-        console.log(`Number of students: ${dt.length}`);
-        const arrayOfClasses = {};
-        arrayOfClasses.count = dt.length;
-        for (const row of dt) {
-          const student = row.split(',');
-          if (!arrayOfClasses[student[3]]) arrayOfClasses[student[3]] = [];
-          arrayOfClasses[student[3]].push(student[0]);
-        }
-        for (const cls in arrayOfClasses) {
-          if (cls && cls !== 'count') console.log(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
-        }
-        res(arrayOfClasses);
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
-    });
-  };
-  return new Promise(promise);
-};
+      const content = data.toString().split('\n');
 
-module.exports = readDatabase;
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+    });
+  });
+}
+
+export default readDatabase;
